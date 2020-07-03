5fef5b9959af3bf534fdba16e47117b87daa1399
Deborah Santillan <s.debb@yahoo.com>
2020-07-03 17:18:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def initialize(name, breed = mutt)
+  def initialize(name, breed = "Mutt")
     @name = dog_n
     @breed = dog_b  
   end
 end 
  
- def name=(name, breed = mutt)
+ def name=(name, breed = "Mutt")
     @name = dog_n
     @breed = dog_b
   end
